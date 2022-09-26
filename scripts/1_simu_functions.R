2431ef4590fcb89a1ea85866ccd4cfd4b5138a5b
Junming Shi <junmingshi@ln003.brc>
2022-09-26 13:37:00 -0700

    update est

--- scripts/1_simu_functions.R
+++ scripts/1_simu_functions.R
@@ -153,12 +153,15 @@ ate_hal_avec0_z01 <- function(a_vec, obs_df){
 
 
 run_simu <- function(n, B){
+  
   ate_hal_a_1_relts_ss_under_hal_simu <- list()
   ate_hal_a_1_relts_ss_hal_simu <- list()
   ate_hal_a_0_relts_ss_under_hal_simu <- list()
   ate_hal_a_0_relts_ss_hal_simu <- list()
   
   for (b in 1:B) {
+    print(paste0("-----starting round ", b, "-----"))
+    
     Obs1_n <- generate_data1(n)
     ate_hal_a_z_n500 <- ate_hal_avec0_z01(a_vec, obs_df=Obs1_n)
     
