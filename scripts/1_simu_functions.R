91e018554d6eaa6cb71a98c548eb88284b07bd26
SeraphinaShi <junmingshi67@gmail.com>
2022-10-12 22:32:12 -0700

    update lambada NA error

--- scripts/1_simu_functions.R
+++ scripts/1_simu_functions.R
@@ -9,7 +9,17 @@ undersmooth_hal_fit <- function(n, X, Y, y_type){
                                      fit_init = hal_init$fit_init,
                                      basis_mat = hal_init$basis_mat,
                                      family = y_type)
-
+  
+  #if(is.na(hal_undersmooth$lambda_under)){browser()}
+  if(is.na(hal_undersmooth$lambda_under)){
+    hal_init <- undersmooth_init(X, Y, family = y_type)
+    hal_undersmooth <- undersmooth_hal(X, Y,
+                                       fit_init = hal_init$fit_init,
+                                       basis_mat = hal_init$basis_mat,
+                                       family = y_type)
+    print("  (Since the learned undersmoothed lambda is NA, refitting lambdas.)")
+  }
+  
   print(paste0("  Fitting initial HAL with lambda:       ", round(hal_undersmooth$lambda_init,5)))
   dgd_hal_fit <- fit_hal(X = X,
                          Y = Y,
