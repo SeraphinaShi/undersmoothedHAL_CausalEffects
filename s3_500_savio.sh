7c918a0663242aa099a6b0e0ed18cf70156e7fd6
Junming Shi <junmingshi@ln002.brc>
2023-04-02 08:04:39 -0700

    try

---
+++ s3_500_savio.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=sys3_n500
+#SBATCH --account=junmingshi
+#SBATCH --partition=savio3
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=50:00:30
+#SBATCH --mail-user=junming_shi@berkeley.edu
+## Command(s) to run:
+
+R CMD BATCH --no-save scripts/scripts_v3/2_simulations_sys3_500.R scripts/scripts_v3/2_simulations_sys3_500.Rout
