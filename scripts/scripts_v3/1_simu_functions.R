546070d4b95484b4523e146d536b96f6be510c0b
SeraphinaShi <junmingshi67@gmail.com>
2023-05-04 09:17:58 -0700

    update

--- scripts/scripts_v3/1_simu_functions.R
+++ scripts/scripts_v3/1_simu_functions.R
@@ -268,7 +268,7 @@ run_simu_1round <- function(gen_data_functions, n, undersmooth='none', lambda_sc
 run_simu_rep <- function(gen_data_functions, n, B, undersmooth='none', lambda_scaler=1, return_all_rslts = F){
   result_list <- list()
   for(b in 1:B){
-    print(b)
+    print(paste0("round ", b))
     result <- tryCatch({
       run_simu_1round(gen_data_functions, n=n, undersmooth, lambda_scaler)
     }, error = function(e) {
