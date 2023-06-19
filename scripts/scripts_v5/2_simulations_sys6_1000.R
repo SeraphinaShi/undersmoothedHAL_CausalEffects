bd50ad47d424a7c9673d4d0a82fa4d23eec7656a
SeraphinaShi <junmingshi67@gmail.com>
2023-06-19 14:04:28 -0700

    fix error

--- scripts/scripts_v5/2_simulations_sys6_1000.R
+++ scripts/scripts_v5/2_simulations_sys6_1000.R
@@ -68,30 +68,34 @@ generate_data_6 <- function(n, a=NA){
 # Getting trul value of psi
 #------------------------------------------------------------------------------------
 
-a_vec <- seq(0, 1, 0.01)
-psi0_a <- c()
-psi0_a <- c()
-
-N = 1e+07
-
-for (i in 1:length(a_vec)) {
-  a <- a_vec[i]
-
-  data_a <- generate_data_6(n=N, a=a)
-  psi0_a[i] <- mean(data_a$Y) # - data_0$Y
-}
-
-psi0_line <- data.frame(a=a_vec, psi0 = psi0_a)
-plot(psi0_line$a, psi0_line$psi0)
-
-eval_points = seq(0,1,0.05)
-
-psi0_pnt <- psi0_line[psi0_line$a %in% eval_points,]
-
-save.image(file=here("data", "rdata", "02_simu_V5_sys6_psi0.RData"))
+# a_vec <- seq(0, 1, 0.01)
+# psi0_a <- c()
+# psi0_a <- c()
+# 
+# N = 1e+07
+# 
+# for (i in 1:length(a_vec)) {
+#   a <- a_vec[i]
+# 
+#   data_a <- generate_data_6(n=N, a=a)
+#   psi0_a[i] <- mean(data_a$Y) # - data_0$Y
+# }
+# 
+# psi0_line <- data.frame(a=a_vec, psi0 = psi0_a)
+# plot(psi0_line$a, psi0_line$psi0)
+# 
+# eval_points = seq(0.05,1,0.05)
+# 
+# psi0_pnt <- psi0_line[psi0_line$a %in% eval_points,]
+# 
+# save.image(file=here("data", "rdata", "02_simu_V5_sys6_psi0.RData"))
 
 
 ## -----------------------------------------------------------------------------------------------------------------------
+load(here("data", "rdata", "02_simu_V5_sys6_psi0.RData"))
+source(here("scripts", "scripts_v5", "1_hal_functions.R"))
+source(here("scripts", "scripts_v5", "1_simu_functions.R"))
+
 n = 1000
 
 set.seed(123)
